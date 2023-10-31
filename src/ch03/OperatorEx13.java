25e3f6a5e15b9f04f8bf0a5081ae4107086a5375
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2023-10-31 13:13:56 +0900

    operator

---
+++ src/ch03/OperatorEx13.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class OperatorEx13 {
+    public static void main(String[] args) {
+        char c1 = 'a';
+
+        //char c2 = c1+1;// 에러
+        char c2 = 'a'+1;//리터럴 간의 연산은 실행 과정 동안 변하는 값이 아니기 때문에 컴파일 시에 컴파일러가 계산해서 그 결과로 대체한다.
+
+        System.out.println(c2);
+    }
+}
