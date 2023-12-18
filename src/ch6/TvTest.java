02f466b395e71b61bbfcf75cf8b140f238012313
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2023-12-18 12:53:44 +0900

    객체

---
+++ src/ch6/TvTest.java
@@ -0,0 +1,22 @@
+package ch6;
+
+class Tv{
+    //tv의 속성(멤버변수)
+    String color;
+    boolean power;
+    int channel;
+
+    //tv의 기능(메서드)
+    void power(){ power=!power;}
+    void channelUp() {++channel;}
+
+    void channelDown(){--channel;}
+}
+public class TvTest {
+    public static void main(String[] args) {
+        Tv t= new Tv();// tv 인스턴스를 생성한 후, 생성된 tv 인스턴스의 주소를 참조변수 t에 저장
+        t.channel=7;
+        t.channelDown();
+        System.out.println("현재 채널은 "+ t.channel+" 입니다.");
+    }
+}
